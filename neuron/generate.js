8463ddfb2651c348ad1f354e8e8e3cce557c2bad
Treeston <treeston.mmoc@gmail.com>
2021-08-06 04:04:03 +0200

    re-wire generate.js

--- neuron/generate.js
+++ neuron/generate.js
@@ -21,43 +21,86 @@ const MASK = (async () =>
     return mask;
 })();
 
-(async () => {
-    const SQL = await window.initSqlJs({ locateFile: ((file) => ('https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.5.0/'+file)) });
-    
-    const statusElm = document.getElementById('status');
-    const fileInput = document.getElementById('input');
-    fileInput.addEventListener('change', async () =>
+const GetMissingArtwork = (async (cardId, knownArtworks) =>
+{
+    let { artworks } = await (await fetch('https://db.ygorganization.com/data/card/'+cardId)).json();
+    if (knownArtworks)
+        artworks = artworks.filter((artId) => !knownArtworks.has(artId));
+    return Promise.all(artworks.map(async (artId) => 
     {
-        const file = fileInput.files[0];
-        if (!file) return;
-        
-        statusElm.innerText = 'Opening DB...';
-        
-        const db = new SQL.Database(new Uint8Array(await file.arrayBuffer()));
-        const results = db.exec('SELECT cardId, artId, artwork FROM card_artwork')[0].values;
-        const nTotal = results.length;
-        
-        const hashes = [];
-        let nDone = 0;
-        const mask = await MASK;
+        return [
+            cardId,
+            artId,
+            await createImageBitmap(await (await fetch('https://db.ygorganization.com/artwork/'+cardId+'/'+artId)).blob())
+        ];
+    }));
+});
+
+const statusElm = document.getElementById('status');
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', async () =>
+{
+    startButton.disabled = true;
+    try
+    {
         await EnsureScriptLoaded('/neuron/cardident.js');
-        for (const [cardId, artId, artwork] of results)
+        
+        statusElm.innerText = 'Loading existing data...';
+        const existing = await (await fetch('/neuron/imagedb.json')).json();
+        statusElm.innerText = 'Processing existing data...';
+        await sleep(0);
+        
+        const cardIndex = {};
+        for (const [cardId, artId, fingerprint] of existing)
         {
-            const artFile = new File([artwork], (cardId+'_'+artId+'.png'), { type: 'image/png' });
-            const artBitmap = await createImageBitmap(artFile);
-            
-            const fingerprint = await CardFingerprint.Fingerprint(artBitmap, null, null, null, null, mask);
-            
-            document.getElementById('bla').getContext('2d').drawImage(artBitmap, 0, 0, 200, 290);
-            CardFingerprint.Visualize(document.getElementById('bla2'), fingerprint);
-            
-            ++nDone;
-            statusElm.innerText = (nDone+'/'+nTotal+' done ('+((nDone*100/nTotal).toFixed(2))+'%)');
-            hashes.push([cardId, artId, fingerprint]);
+            const cardData = (cardIndex[cardId] || (cardIndex[cardId] = { artworks: new Set() }));
+            cardData.artworks.add(artId);
         }
         
+        statusElm.innerText = 'Fetching artwork count index...';
+        const artIndex = Object.entries(await (await fetch ('https://db.ygorganization.com/data/idx/card/artwork_count')).json());
+        statusElm.innerText = 'Processing artwork count index...';
+        await sleep(0);
+        
+        const promises = [];
+        for (const [count, cards] of artIndex)
+        {
+            for (const cardId of cards)
+            {
+                if (cardId <= 0) continue;
+                const ourIdx = (cardIndex[cardId] && cardIndex[cardId].artworks);
+                if (ourIdx && (ourIdx.size >= count))
+                    continue;
+                promises.push(GetMissingArtwork(cardId, ourIdx));
+            }
+        }
+        
+        statusElm.innerText = 'Requesting missing artwork...';
+        const artworks = await Promise.all(promises);
+        
+        const mask = await MASK;
+        const nTotal = artworks.reduce((a,c) => a+c.length, 0);
+        let nDone = 0;
+        for (const arr of artworks)
+        {
+            for (const [cardId, artId, bitmap] of arr)
+            {
+                const fingerprint = await CardFingerprint.Fingerprint(bitmap, null, null, null, null, mask);
+            
+                document.getElementById('bla').getContext('2d').drawImage(bitmap, 0, 0, 200, 290);
+                CardFingerprint.Visualize(document.getElementById('bla2'), fingerprint);
+                
+                ++nDone;
+                statusElm.innerText = (nDone+'/'+nTotal+' done ('+((nDone*100/nTotal).toFixed(2))+'%)');
+                existing.push([cardId, artId, fingerprint]);
+                await sleep(0);
+            }
+        }
         statusElm.innerText = 'Done, offering download';
         
-        DOWNLOAD('imagedb.json', 'data:text/plain;charset=utf-8,'+encodeURIComponent(JSON.stringify(hashes)+'\n'));
-    });
-})();
+        DOWNLOAD('imagedb.json', 'data:text/plain;charset=utf-8,'+encodeURIComponent(JSON.stringify(existing)+'\n'));
+    } catch (e) {
+        console.error(e);
+        statusElm.innerText = ('Failed: '+e);
+    } finally { startButton.disabled = false; }
+});
